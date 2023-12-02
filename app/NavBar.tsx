af2459ab775ed469a37dd30a6c90374597bd0601
fayrouz_hussien <fayrouz.mohamed@gizasystems.com>
2023-12-02 10:29:53 +0200

    styling active path

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,9 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { IoBug } from "react-icons/io5";
+import classNames from 'classnames';
 const NavBar = () => {
     const Links=[
         {
@@ -12,12 +15,20 @@ const NavBar = () => {
             title:'issues'
         }
     ]
+    const currrentpath=usePathname()
   return (
     <nav className='border-solid border-y-2 border-gray-600 flex space-x-6 align-middle mb-6'>
         <Link href='/' className='p-2'><IoBug /></Link>
         <ul className='space-x-6 flex p-1'>
             {
-                Links.map(link=><Link className='text-zinc-500 hover:text-zinc-800 transition-colors' key={link.href} href={link.href}>{link.title}</Link>)
+                Links.map(link=><Link className={classNames(
+                    {
+                        'text-zinc-500':currrentpath!=link.href,
+                        'hover:text-zinc-800 transition-colors':true,
+                        'text-zinc-900':currrentpath==link.href,
+
+                    }
+                )}  key={link.href} href={link.href}>{link.title}</Link>)
             }
         </ul>
 
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+const issuesPage = () => {
+    
+  return (
+    <div>issuespage</div>
+  )
+}
+
+export default issuesPage
