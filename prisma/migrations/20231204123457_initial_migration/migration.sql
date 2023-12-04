b7b94d9b8f2bf135446c50a82249b1aa224669d1
unknown <fayrouzhussien21@gmail.com>
2023-12-04 15:41:40 +0300

    create migration

---
+++ prisma/migrations/20231204123457_initial_migration/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `issue` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `status` ENUM('OPEN', 'CLOSE', 'IN_PROGRESS') NOT NULL DEFAULT 'OPEN',
+    `updateAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
